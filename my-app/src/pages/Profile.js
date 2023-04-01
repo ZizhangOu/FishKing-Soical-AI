6d3863352f2ee48ad8f8d6872b38f32aaf0781bb
katherineqh <57205225+katherineqh@users.noreply.github.com>
2023-04-01 21:00:33 +1100

    add routes

--- my-app/src/pages/Profile.js
+++ my-app/src/pages/Profile.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Navbar from "components/Navbars/AuthNavbar.js";
+import Navbar from "../components/AutuNavbar";
 
 
 export default function Profile() {
--- my-app/src/routes/appRoutes.js
+++ my-app/src/routes/appRoutes.js
@@ -2,12 +2,14 @@ import {Login} from '../pages/Login';
 import {Register} from '../pages/Register';
 
 import Home from '../pages/Home';
+import Profile from '../pages/Profile';
 
 import { Navigate } from 'react-router-dom';
 
 export const appRoutes = [
   { path: '/signup', element: <Register /> },
   { path: '/login', element: <Login /> },
+  { path: '/profile', element: <Profile /> },
 
   // {
   //   path: '/dashboard',
