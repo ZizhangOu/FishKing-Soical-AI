f68b65a237175928271dc843265b56317c2978d9
Zizhang Ou Kevin Zen <106296608+ZizhangOu@users.noreply.github.com>
2023-04-02 01:36:36 +0000

    Swipe Finish

--- my-app/src/pages/Dashboard.js
+++ my-app/src/pages/Dashboard.js
@@ -1,5 +1,47 @@
 import React, { useState } from 'react';
 import BottomNav from '../components/BottomNavBar';
+import TinderCard from 'react-tinder-card';
+import './App.css';
+
+const db = [
+  {
+    name: 'Richard Hendricks',
+    url: 'https://i.imgur.com/MWAcQRM.jpeg'
+  },
+  {
+    name: 'Erlich Bachman',
+    url: 'https://i.imgur.com/Lnt9K7l.jpeg'
+  },
+  {
+    name: 'Monica Hall',
+    url: 'https://i.imgur.com/Q9WPlWA.jpeg'
+  },
+  {
+    name: 'Jared Dunn',
+    url: 'https://i.imgur.com/Gg6BpGn.jpeg'
+  },
+  {
+    name: 'Dinesh Chugtai',
+    url: 'https://i.imgur.com/clq9lGO.jpeg'
+  },
+  {
+    name: 'Erlich Bachman',
+    url: 'https://i.imgur.com/Lnt9K7l.jpeg'
+  },
+  {
+    name: 'Monica Hall',
+    url: 'https://i.imgur.com/Q9WPlWA.jpeg'
+  },
+  {
+    name: 'Jared Dunn',
+    url: 'https://i.imgur.com/Gg6BpGn.jpeg'
+  },
+  {
+    name: 'Dinesh Chugtai',
+    url: 'https://i.imgur.com/clq9lGO.jpeg'
+  }
+  
+]
 
 
 function Dashboard() {
@@ -9,10 +51,37 @@ function Dashboard() {
     setActiveTab(tab);
   };
 
+  const characters = db
+  const [lastDirection, setLastDirection] = useState()
+
+  const swiped = (direction, nameToDelete) => {
+    console.log('removing: ' + nameToDelete)
+    setLastDirection(direction)
+  }
+
+  const outOfFrame = (name) => {
+    console.log(name + ' left the screen!')
+  }
+
   return (
+
     <div className="flex flex-col h-screen">
       <div className="flex-grow">
-
+      <div>
+      <link href='https://fonts.googleapis.com/css?family=Damion&display=swap' rel='stylesheet' />
+      <link href='https://fonts.googleapis.com/css?family=Alatsi&display=swap' rel='stylesheet' />
+      <h1>React Tinder Card</h1>
+      <div className='cardContainer'>
+        {characters.map((character) =>
+          <TinderCard className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
+            <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
+              <h3>{character.name}</h3>
+            </div>
+          </TinderCard>
+        )}
+      </div>
+      {lastDirection ? <h2 className='infoText'>You swiped {lastDirection}</h2> : <h2 className='infoText' />}
+    </div>
       </div>
       <BottomNav activeTab={activeTab} onChangeTab={onChangeTab} />
     </div>
@@ -23,6 +92,3 @@ function Dashboard() {
 
   export default Dashboard;
 
-
-
-  
--- my-app/src/routes/appRoutes.js
+++ my-app/src/routes/appRoutes.js
@@ -8,13 +8,15 @@ import Star from '../pages/Star';
 import BottomNavBar from '../components/BottomNavBar';
 
 import { Navigate } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
 
 export const appRoutes = [
   { path: '/signup', element: <Register /> },
   { path: '/login', element: <Login /> },
   { path: '/profile', element: <Profile /> },
-
   { path: '/Star', element: <Star /> },
+  { path: '/Dashboard', element: <Dashboard /> },
+
 
 
   {
