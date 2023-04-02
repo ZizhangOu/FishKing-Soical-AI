e637788b73dab4436296f93df7cddc152b8cc7fc
Zizhang Ou Kevin Zen <106296608+ZizhangOu@users.noreply.github.com>
2023-04-02 03:01:19 +0000

    update profile

--- my-app/src/pages/Profile.js
+++ my-app/src/pages/Profile.js
@@ -9,40 +9,7 @@ export default function Profile() {
     <>
       <Navbar transparent />
       <main className="profile-page">
-      <section className="relative block" style={{ height: "500px" }}>
-
-          <div
-            className="absolute top-0 w-full h-full bg-center bg-cover"
-            style={{
-              backgroundImage:
-                "url('https://images.unsplash.com/photo-1499336315816-097655dcfbda?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2710&q=80')",
-            }}
-          >
-            <span
-              id="blackOverlay"
-              className="w-full h-full absolute opacity-50 bg-black"
-            ></span>
-          </div>
-          <div
-            className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-16"
-            style={{ transform: "translateZ(0)" }}
-          >
-            <svg
-              className="absolute bottom-0 overflow-hidden"
-              xmlns="http://www.w3.org/2000/svg"
-              preserveAspectRatio="none"
-              version="1.1"
-              viewBox="0 0 2560 100"
-              x="0"
-              y="0"
-            >
-              <polygon
-                className="text-blueGray-200 fill-current"
-                points="2560 0 2560 100 0 100"
-              ></polygon>
-            </svg>
-          </div>
-        </section>
+      
         <section className="relative py-16 bg-blueGray-200">
           <div className="container mx-auto px-4">
             <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg ">
@@ -52,8 +19,8 @@ export default function Profile() {
                     <div className="relative">
                       <img
                         alt="..."
-                        src="https://i.imgur.com/Q9WPlWA.jpg"
-                        className="shadow-xl rounded-full h-auto align-middle border-none mt-20 lg:mt-16 max-w-150-px z-10"
+                        src="https://i.imgur.com/oPj4A8u.jpeg"
+                        className="shadow-xl rounded-full h-auto align-middle border-none mt-20 lg:mt-16 max-w-25-px max-h-75-px z-10"
                         
                       />
                     </div>
